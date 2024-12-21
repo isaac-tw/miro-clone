3711cbace8e9d113d58afc10474e01050cb32226
Isaac Huang <isaac.huang.tw@gmail.com>
2024-12-21 21:07:02 +0800

    Add basic layout

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -1,38 +1,41 @@
-"use client"
+"use client";
 
-import { AuthLoading, Authenticated, ConvexReactClient, Unauthenticated } from "convex/react";
 import {
-    ClerkProvider,
-    SignInButton,
-    useAuth,
-    UserButton
+  AuthLoading,
+  Authenticated,
+  ConvexReactClient,
+  Unauthenticated,
+} from "convex/react";
+import {
+  ClerkProvider,
+  SignInButton,
+  useAuth,
 } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import Loading from "@/components/auth/loading";
 
 interface ConvexClientProviderProps {
-    children: React.ReactNode;
-};
+  children: React.ReactNode;
+}
 
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
 const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 const convex = new ConvexReactClient(convexUrl);
 
-export const ConvexClientProvider = ({ children }: ConvexClientProviderProps) => {
-    return (
-        <ClerkProvider publishableKey={publishableKey}>
-            <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
-                <AuthLoading>
-                    <Loading />
-                </AuthLoading>
-                <Unauthenticated>
-                    <SignInButton />
-                </Unauthenticated>
-                <Authenticated>
-                    <UserButton />
-                    {children}
-                </Authenticated>
-            </ConvexProviderWithClerk>
-        </ClerkProvider>
-    );
+export const ConvexClientProvider = ({
+  children,
+}: ConvexClientProviderProps) => {
+  return (
+    <ClerkProvider publishableKey={publishableKey}>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
+        <Unauthenticated>
+          <SignInButton />
+        </Unauthenticated>
+        <Authenticated>{children}</Authenticated>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 };
