410ec382746a1b88402a09481b3c53e6c3f02317
Isaac Huang <isaac.huang.tw@gmail.com>
2024-12-28 21:32:29 +0800

    Room Info

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,7 +1,81 @@
-const Info = () => {
+"use client";
+
+import Actions from "@/components/actions";
+import Hint from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import { useRenameModal } from "@/store/use-rename-modal";
+import { useQuery } from "convex/react";
+import { MenuIcon } from "lucide-react";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+interface InfoProps {
+  boardId: string;
+}
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+const TabSeparator = () => {
+  return <div className="px-1.5 text-neutral-300">|</div>;
+};
+
+const Info = ({ boardId }: InfoProps) => {
+  const { onOpen } = useRenameModal();
+
+  const data = useQuery(api.board.get, {
+    id: boardId as Id<"boards">,
+  });
+
+  if (!data) return <InfoSkeleton />;
+
+  const handleRename = () => {
+    onOpen(data._id, data.title);
+  };
+
   return (
     <div className="absolute left-2 top-2 flex h-12 items-center rounded-md bg-white px-1.5 shadow-md">
-      Info
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button variant="board" className="px-2" asChild>
+          <Link href="/">
+            <Image src="/logo.svg" alt="Logo" width={30} height={30} />
+            <span
+              className={cn(
+                "ml-2 text-xl font-semibold text-black",
+                font.className,
+              )}
+            >
+              Board
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          variant="board"
+          className="px-2 text-base font-normal"
+          onClick={handleRename}
+        >
+          {data.title}
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Actions id={data._id} title={data.title} side="bottom" sideOffset={10}>
+        <div>
+          <Hint label="Main menu" side="bottom" sideOffset={10}>
+            <Button size="icon" variant="board">
+              <MenuIcon />
+            </Button>
+          </Hint>
+        </div>
+      </Actions>
     </div>
   );
 };
